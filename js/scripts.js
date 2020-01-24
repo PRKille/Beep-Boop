4949baa5a1b51095f3f225bb65feb11671f5e5fb
Patrick Kille <pr.kille@gmail.com>
2020-01-24 10:53:31 -0800

    replicate number replacement function for numbers 1 and 2

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,10 @@ function beepboop(inputNumber) {
   for(let i = 0; i <= parseInt(inputNumber); i++) {
     arrNumbers.push(i);
   }
-  var arrStrings = arrNumbers.map(String)
-  arrStrings = replaceThree(arrStrings)
+  var arrStrings = arrNumbers.map(String);
+  replaceThree(arrStrings);
+  replaceTwo(arrStrings);
+  replaceOne(arrStrings);
   return arrStrings;
 }
 
@@ -13,6 +15,22 @@ function replaceThree(threeNumbers) {
   threeNumbers.forEach(function(threeNumber, index, arr){
     if (threeNumber.includes("3")){
       arr[index] = "I'm sorry, Dave. I'm afraid I can't do that."
+    }
+  });
+}
+
+function replaceTwo(twoNumbers) {
+  twoNumbers.forEach(function(twoNumber, index, arr){
+    if (twoNumber.includes("2")){
+      arr[index] = "Boop"
+    }
+  });
+}
+
+function replaceOne(oneNumbers) {
+  oneNumbers.forEach(function(oneNumber, index, arr){
+    if (oneNumber.includes("1")){
+      arr[index] = "Beep"
     }
   });
 }
